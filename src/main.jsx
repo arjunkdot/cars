f6ebca640e49a45b5418432356a213098cc99552
arjunkdot <78548960+arjunkdot@users.noreply.github.com>
2024-03-10 09:32:19 +0530

    feat: add redux store

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./store";
+const el = document.getElementById("root");
+const root = createRoot(el);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+root.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>
+);
